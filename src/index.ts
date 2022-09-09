0d95f5847282017a233c21b97d63f3290e6092f5
wighawag <wighawag@gmail.com>
2022-09-09 13:45:04 +0100

    use simple processor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import {
   EventWithId,
   Database,
-  SingleEventProcessorObject,
-  PutAndGetDatabase,
-  fromSingleEventProcessorObject,
+  SingleEventProcessorWithBatchSupport,
+  Dependency,
+  SyncDB,
+  fromSingleEventProcessorWithBatchSupportObject,
 } from 'ethereum-indexer-processors';
 
 import { logs } from 'named-logs';
@@ -15,7 +16,11 @@ const console = logs('ERC721EventProcessor');
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-const ERC721EventProcessor: SingleEventProcessorObject = {
+function dbIDForToken(tokenContract: string, tokenID: string): string {
+  return `Token_${tokenContract}_${tokenID}`;
+}
+
+const ERC721EventProcessor: SingleEventProcessorWithBatchSupport & any = {
   async setup(db: Database): Promise<void> {
     await db.setup({
       indexes: [
@@ -36,44 +41,58 @@ const ERC721EventProcessor: SingleEventProcessorObject = {
     });
   },
 
-  async onTransfer(db: PutAndGetDatabase, event: EventWithId): Promise<void> {
-    const to = event.args.to as string;
-    const from = event.args.from as string;
-    const tokenID = event.args.id as string;
-    const tokenContract = event.address;
-    const id = `Token_${tokenContract}_${tokenID}`;
+  onTransfer: {
+    // return the list of ids to fetch from DB
+    // this will be executed for all events received in batch
+    // allow the processor to batch fetch before processing, speeding up the whole operation
+    // it return an array composed of id or function that can fetch further id
+    // DO no support recusrive depndencies yet
+    dependencies(event: EventWithId): Dependency[] {
+      const tokenID = event.args.id as string;
+      const tokenContract = event.address;
+      const id = `Token_${tokenContract}_${tokenID}`;
+      return [id];
+    },
+    // do the actual process and it operate synchronously
+    processEvent(db: SyncDB, event: EventWithId) {
+      const to = event.args.to as string;
 
-    let token = await db.get<Token>(id);
+      const tokenID = event.args.id as string;
+      const tokenContract = event.address;
+      const id = dbIDForToken(tokenContract, tokenID);
 
-    if (!token) {
-      console.info(`new token ${id}: with owner: ${to}`);
-      token = {
-        _id: id,
-        kind: 'Token',
-        owner: to,
-        tokenID,
-        tokenContract,
-      };
-    } else {
-      console.info(`token ${id} already exists`);
-      if (to === ZERO_ADDRESS) {
-        console.info(`deleting it...`);
-        await db.delete(id);
-        return;
+      let token = db.get<Token>(id);
+
+      if (!token) {
+        console.info(`new token ${id}: with owner: ${to}`);
+        token = {
+          _id: id,
+          kind: 'Token',
+          owner: to,
+          tokenID,
+          tokenContract,
+        };
       } else {
-        console.info(`setting new owner: ${to}`);
-        token.owner = to;
+        console.info(`token ${id} already exists`);
+        if (to === ZERO_ADDRESS) {
+          console.info(`deleting it...`);
+          db.delete(id);
+          return;
+        } else {
+          console.info(`setting new owner: ${to}`);
+          token.owner = to;
+        }
       }
-    }
 
-    await db.put(token);
+      db.put(token);
+    },
   },
 };
 
 // we export a factory function called processor
 // the helper "fromSingleEventProcessorObject" will transform the single event processor...
 // ... into the processor type expected by ethereum-indexer-server
-export const processor = fromSingleEventProcessorObject(() => ERC721EventProcessor);
+export const processor = fromSingleEventProcessorWithBatchSupportObject(() => ERC721EventProcessor);
 
 // we expose contractsData as generic to be used on any chain
 export const contractsData = {
